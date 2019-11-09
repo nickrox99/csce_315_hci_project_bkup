37587ecf6f113f688a72d2b7d1cab6cd054e1377
Nicholas Cashiola <nick.cashiola@gmail.com>
2019-11-09 14:26:11 -0600

    Added functions to trending.js to support output of trending tweets [Nick]

---
+++ proj/csce_hci_proj/WebContent/trending.js
@@ -0,0 +1,37 @@
+window.onload = (function(){
+
+    var tweet = document.getElementById("tweet");
+    var id = tweet.getAttribute("tweetID");
+
+    twttr.widgets.createTweet(
+      id, tweet, 
+      {
+        conversation : 'none',    // or all
+        cards        : 'hidden',  // or visible 
+        linkColor    : '#cc0000', // default is blue
+        theme        : 'light'    // or dark
+      })
+    .then (function (el) {
+      el.contentDocument.querySelector(".footer").style.display = "none";
+    });
+
+  });
+
+  function fetchTweets(q) {
+
+    var yql  = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22";
+    var base = "https://twitter.com/i/search/timeline?f=realtime&src=typd&include_entities=0&q=";
+  
+    // Test the URL in YQL console to make sure it works
+    var url  = yql + base + encodeURIComponent(q) + "%22&format=json";
+  
+     // Make synchronous AJAX request to yql
+    var tweets = jQuery.ajax({type: "GET", url: url, dataType: 'json', async: false }).responseText;
+  
+    // Parse the JSON response
+    var data = JSON.parse(tweets);
+  
+    // Return the HTML search results
+    return data.query.results.json.items_html;
+    
+  }
