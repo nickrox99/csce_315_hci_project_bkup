bf11f0791812bdcd5f84cb9d14ff9d0c79cc3582
Nicholas Cashiola <nick.cashiola@gmail.com>
2019-11-09 21:06:33 -0600

    successfully implemented search input [Nick]

--- proj/csce_hci_proj/WebContent/home.js
+++ proj/csce_hci_proj/WebContent/home.js
@@ -1,10 +1,15 @@
+
+
 function getUserSearch()
   {
       // get the user search from the homepage and put it in the globle variable
-      var searchInput = document.getElementById('search_field').nodeValue;
+      var searchInput = document.getElementById('search_field').value;
       result = searchInput;
+      console.log(result);
 
   }
 
-var subButton = document.getElementById('subButton');
-subButton.addEventListener('click', getUserName, false);
+var submit_button = document.getElementById('submit_button');
+if(submit_button){
+    submit_button.addEventListener('click', getUserSearch, true);
+}
