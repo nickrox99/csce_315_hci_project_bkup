6cefaa7b4addaa4bb6ac1f6357a7b80f3d7ec22d
donaldhacker <donaldhacker@tamu.edu>
2019-11-17 22:06:22 -0600

    Added code for all general info request
    Includes: open, high, low, current price, last trading day, previous close, change, and change percentage

--- proj/WebContent/public/scripts/finance.js
+++ proj/WebContent/public/scripts/finance.js
@@ -19,67 +19,84 @@ function loadFinanceInfo(){
     var symbol = 'AMZN'; //test with BTC for currency
 
     var apiKey = '0SE9COWFX0MGZGAE';
-
-    var functionStocks = 'TIME_SERIES_DAILY';
+    var functionStockDaily = 'GLOBAL_QUOTE';
     var interval = '1min';
-    var urlStocks = 'https://www.alphavantage.co/query?function=' + functionStocks + '&symbol=' + symbol + '&interval=' + interval + '&apikey=' + apiKey; 
+    var urlStocks = 'https://www.alphavantage.co/query?function=' + functionStockDaily + '&symbol=' + symbol + '&interval=' + interval + '&apikey=' + apiKey; 
+
+
+    var open = '';
+    var high = '';
+    var low = '';
+    var price = '';
+    var latestTradingDay = '';
+    var previousClose = '';
+    var change = '';
+    var changePercent = '';
+
+    var yearHigh = '';
+    var yearLow = '';
+
+    jQuery.ajax({
+        url: urlStocks,
+        dataType: 'json',
+        contentType: "application/json",
+        success: function(data){
+            console.log(data);
+
+          
+            latestTradingDay = data['Global Quote']['07. latest trading day'];
+            console.log(latestTradingDay);
+            open = data['Global Quote']['02. open'];
+            console.log("Open: " + open);
+            high = data['Global Quote']['03. high'];
+            console.log("High: " + high);
+            low = data['Global Quote']['04. low'];
+            console.log("Low: " + low);
+            price = data['Global Quote']['05. price'];
+            console.log("Close: " + price);
+            change = data['Global Quote']['09. change'];
+            console.log("Change: " + change);
+            changePercent = data['Global Quote']['10. change percent'];
+            console.log("Change Percent: " + changePercent);
+          
+        }
+
+    });
+
+    //Code for 
+
+    // var functionCrypto = 'DIGITAL_CURRENCY_WEEKLY';
+    // var market = 'USD';
+    // var urlCrypto = 'https://www.alphavantage.co/query?function=' + functionCrypto + '&symbol=' + symbol + '&market=' + market + '&apikey=' + apiKey; 
 
     // jQuery.ajax({
-    //     url: urlStocks,
+    //     url: urlCrypto,
     //     dataType: 'json',
     //     contentType: "application/json",
     //     success: function(data){
     //         console.log(data);
+            
+    //         name = data['Meta Data']['3. Digital Currency Name'];
+    //         date = data['Meta Data']['6. Last Refreshed'];
+    //         date = date.substr(0,10);
+    //         open = data['Time Series (Digital Currency Weekly)'][date]['1a. open (USD)'];
+    //         high = data['Time Series (Digital Currency Weekly)'][date]['2b. high (USD)'];
+    //         low = data['Time Series (Digital Currency Weekly)'][date]['3a. low (USD)'];
+    //         close = data['Time Series (Digital Currency Weekly)'][date]['4a. close (USD)'];
 
-          
-    //         date = data['Meta Data']['3. Last Refreshed'];
-    //         open = data['Time Series (Daily)'][date]['1. open'];
-    //         high = data['Time Series (Daily)'][date]['2. high'];
-    //         low = data['Time Series (Daily)'][date]['3. low'];
-    //         close = data['Time Series (Daily)'][date]['4. close'];
-
-    //         console.log('Company Symbol: ' + symbol);
+    //         console.log('Currency Name: ' + name);
+    //         console.log('Currency Symbol: ' + symbol);
     //         console.log('Date: ' + date);
     //         console.log('Open: $' + open);
     //         console.log('High: $' + high);
     //         console.log('Low: $' + low);
     //         console.log('Close: $' + close);
-          
-         
     //     }
     // });
-
-    var functionCrypto = 'DIGITAL_CURRENCY_WEEKLY';
-    var market = 'USD';
-    var urlCrypto = 'https://www.alphavantage.co/query?function=' + functionCrypto + '&symbol=' + symbol + '&market=' + market + '&apikey=' + apiKey; 
-
-    jQuery.ajax({
-        url: urlCrypto,
-        dataType: 'json',
-        contentType: "application/json",
-        success: function(data){
-            console.log(data);
-            
-            name = data['Meta Data']['3. Digital Currency Name'];
-            date = data['Meta Data']['6. Last Refreshed'];
-            date = date.substr(0,10);
-            open = data['Time Series (Digital Currency Weekly)'][date]['1a. open (USD)'];
-            high = data['Time Series (Digital Currency Weekly)'][date]['2b. high (USD)'];
-            low = data['Time Series (Digital Currency Weekly)'][date]['3a. low (USD)'];
-            close = data['Time Series (Digital Currency Weekly)'][date]['4a. close (USD)'];
-
-            console.log('Currency Name: ' + name);
-            console.log('Currency Symbol: ' + symbol);
-            console.log('Date: ' + date);
-            console.log('Open: $' + open);
-            console.log('High: $' + high);
-            console.log('Low: $' + low);
-            console.log('Close: $' + close);
-        }
-    });
     
 }
 
+//add listener function
 function symbolSearch() {
 
     var apiKey = '0SE9COWFX0MGZGAE';
@@ -100,13 +117,13 @@ function symbolSearch() {
 window.onload = (function () {
    
     // load the finance info
-    //loadFinanceInfo();
+    loadFinanceInfo();
 
     // get the search results from local storage
-    getNewSearchResult();
+    //getNewSearchResult();
 
     // symbol search
-    symbolSearch()
+    //symbolSearch()
 
 
 
