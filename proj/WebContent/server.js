c71260ddb9bbf2bd122c94432a2fc49283b3a813
Nicholas Cashiola <nick.cashiola@gmail.com>
2019-11-19 17:06:34 -0600

    added basic routing logic, trying to test it [Nick]

--- proj/WebContent/server.js
+++ proj/WebContent/server.js
@@ -1,17 +1,47 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
 var app = express();
-app.use(express.static('public'));
+
+
+// ROUTER
+router.get('/', function (req, res) {
+    res.sendFile("/public/home.html");
+    console.log("home.html loaded");
+
+});
+router.get('/trending', function (req, res) {
+    res.sendFile(path.join(__dirname + '/trending.html'));
+    console.log("trending.html loaded");
+
+});
+router.get('/finance', function (req, res) {
+    res.sendFile(path.join(__dirname + '/finance.html'));
+    console.log("finance.html loaded");
+
+});
+router.get('/about-us', function (req, res) {
+    res.sendFile(path.join(__dirname + '/about-us.html'));
+    console.log("about-us.html loaded");
+
+});
+
+
+
+
+//app.use(express.static('public'));
+//app.use('/', indexRouter);
 var port = 8000; // you can use any port
 app.listen(port, '127.0.0.1');
 
 
-app.get("/trending.html", loadTwitter);
+//app.get("/trending.html", loadTwitter);
 console.log('server on' + port);
 
 
-function loadTwitter()
-{
+function loadTwitter() {
     console.log("loadTwitter() in server.js called");
 }
 
