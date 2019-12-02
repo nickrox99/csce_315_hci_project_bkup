2e93b31d2152096942ab22935f2304310e291bd8
Nicholas Cashiola <nick.cashiola@gmail.com>
2019-12-02 16:05:02 -0600

    Heroku run time error -> render engine not set  [Nick]

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
@@ -32,15 +26,5 @@ app.listen(port, host, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
