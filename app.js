c551b7bab15db210a03e5a9290f988cd51e42ad4
Nicholas Cashiola <nick.cashiola@gmail.com>
2019-12-03 15:22:37 -0600

    added back-end search functionality [Nick]

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var Twitter = require('twit');
 var client = new Twitter({
   consumer_key: 'M5KE9w5g3o1nvohfsJoHMpy6p ',
   consumer_secret: 'HyqjN5Mw8LeDwkjmeRRUaiygTRUink6vlH9XzEAeTDFNLcl6vm',
-  app_only_auth:        true
+  app_only_auth: true
 });
 
 //ar indexRouter = require('./routes/index');
@@ -37,55 +37,76 @@ app.use(cookieParser());
 
 //app.use('/', indexRouter);
 
-app.get('/', function(req, res)
-{
+app.get('/', function (req, res) {
   console.log("redirecting to home page");
   res.redirect('/home');
 });
 
-app.get('/home', function(req, res)
-{
+app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname + "/views/home.html"));
   //res.json({tweet: 'Test'});
 });
 
-app.get('/trending.html', function(req, res)
-{
+app.get('/trending.html', function (req, res) {
   console.log("redirecting to trending page");
   res.redirect('/trending');
 });
 
-app.get('/trending', function(req, res)
-{
+// search logic to receive search results from front-end
+app.post('/trending', (req, res) => {
+  if(typeof req.body.bar === 'undefined')
+  {
+    res.status(400).json({error : 'missing paramter bar', data: null});
+    return;
+  }
+  let bar = req.body.bar;
+  res.status(200).json({error: null, data: bar});
+});
+
+app.get('/trending', function (req, res) {
   res.sendFile(path.join(__dirname + "/views/trending.html"));
 
   // twitter search
-  client.get('search/tweets' , {q: 'apple'}, function(error, data, response)
-  {
-    if(error)
-    {
+  client.get('search/tweets', { q: 'apple' }, function (error, data, response) {
+    // TODO: fix results, currently only getting 'null'
+    if (error) {
       console.log("[LOG] No error:  " + data);
     }
-    if(!error)
-    {
+    if (!error) {
       console.log("[LOG] Error: " + data);
     }
   });
 
   // wikipedia search
+  console.log("loadWiki() started in trending.js");
+  var request = new XMLHttpRequest();
 
-  // TODO put wiki logic here
+  var query = 'test';
+  var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search="' + result + '&format=json&callback=?&origin=*';
+  request.open('GET', url, true);
+
+  request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+  request.onload = function () {
+
+    // access JSON data
+    //var data = JSON.parse(request.responseText)
+
+    console.log(request.response);
+  }
+  request.send();
+
 
   // finance search
 
-  // TODO put finance logic here
-  
+  // TODO: put finance logic here
+
 });
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.listen(port, host, function(){
+app.listen(port, host, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
