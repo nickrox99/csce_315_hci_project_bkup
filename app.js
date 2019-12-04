92a2e2e10c448498ef6bace4e1ad2ba4ae893367
donaldhacker <donaldhacker@tamu.edu>
2019-12-04 17:56:27 -0600

    partial graph implementation

--- app.js
+++ app.js
@@ -179,19 +179,16 @@ app.get('/financeAPIcall', function(req, res){
 app.get('/graphFinanceAPIcall', function(req, res){
   console.log("[LOG] /graphFinanceAPIcall started");
 
-  // TODO add finance API call logic
   var jsonReponse;
   var request = new XMLHttpRequest();
-  var url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=DJIA&apikey=0SE9COWFX0MGZGAE&format=json&callback=?&origin=*';
+  var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DJIA&apikey=0SE9COWFX0MGZGAE&format=json&callback=?&origin=*';
   request.responseType = 'json';
   request.open('GET', url, true);
   request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
   request.onload = function()
   {
-    jsonReponse = request.responseText;
-    console.log(jsonReponse);
+    jsonReponse = JSON.parse(request.responseText);
     res.json(jsonReponse);
-    
   }
   request.send();  
 
--- client/src/App.js
+++ client/src/App.js
@@ -116,26 +116,9 @@ class financeGraphInfo extends React.Component {
   }
   render(){
     const { error, isLoaded, items } = this.state;
-    var data = String(this.state.items)
-    console.log(data)
-    var correctedData = data.replace(/{/g,'').replace(/}/g,'').replace(/"Global Quote":/g,'').replace(/"01. symbol":/g,'').replace(/"02. open":/g,'').replace(/"03. high":/g,'')
-    .replace(/"04. low":/g,'').replace(/"05. price":/g,'').replace(/"06. volume":/g,'').replace(/"07. latest trading day":/g,'').replace(/"08. previous close":/g,'')
-    .replace(/"09. change":/g,'').replace(/"10. change percent":/g,'').replace(/"/g,'')
-    var correctedDataArray = correctedData.split(",")
-
-    const element = 
-      React.createElement('p',{},"Ticker: " + String(correctedDataArray[0]),
-      React.createElement('p',{},"Open: " + String(correctedDataArray[1]),
-      React.createElement('p',{},"High: " + String(correctedDataArray[2]),
-      React.createElement('p',{},"Low: " + String(correctedDataArray[3]),
-      React.createElement('p',{},"Price: " + String(correctedDataArray[4]),
-      React.createElement('p',{},"Volume: " + String(correctedDataArray[5]),
-      React.createElement('p',{},"Disclamer: " + String(correctedDataArray[6]),
-      React.createElement('p',{},"Previous Close: " + String(correctedDataArray[7]),
-      React.createElement('p',{},"Change: " + String(correctedDataArray[8]),
-      React.createElement('p',{},"Change Percent: " + String(correctedDataArray[9])
-    ))))))))))
-    return element
+    console.log(this.state.items)
+    console.log(this.state.items)
+    return "Finance Graphs"
   }
 }
 class twitterInfo extends React.Component {
